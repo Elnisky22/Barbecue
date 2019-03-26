c77107ae9303ff8da505473e5b99f1abc1230634
Elnisky <leonardoelnisky@hotmail.com>
2019-03-26 20:16:57 -0300

    Update IConst.java

--- dao/IConst.java
+++ dao/IConst.java
@@ -1,12 +1,12 @@
 package dao;
 
 /**
- * Interface usada para a conex„o com o banco de dados.
+ * Interface usada para a conex√£o com o banco de dados.
  * @author Darvyn Santos, Eduardo G. Pires, Leonardo Elnisky
  *
  */
 public interface IConst {
 	public static final String stringDeConexao = "jdbc:postgresql://localhost:5433/Barbecue";
 	public static final String usuario = "postgres";
-	public static final String senha = "leonardo12";
+	public static final String senha = "********";
 }
